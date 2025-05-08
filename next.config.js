579dd26e4054d8647b37d20318a93447a168d883
MochieCubix <110340036+MochieCubix@users.noreply.github.com>
2025-05-08 15:56:23 +1000

    Vercel 2

---
+++ next.config.js
@@ -0,0 +1,52 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  // Skip static optimization
+  staticPageGenerationTimeout: 1000,
+  // Use tmp directory for cache
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  images: {
+    unoptimized: true,
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
+        port: ""
+      }
+    ]
+  }
+};
+
+module.exports = nextConfig; 
